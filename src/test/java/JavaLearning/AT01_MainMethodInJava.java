5226f084f9cf13abd2cd30cf9728032b3e9a570f
softwaretestlab <35885117+softwaretestlab@users.noreply.github.com>
2023-02-10 13:43:27 +1100

    Update AT01_MainMethodInJava.java

--- src/test/java/JavaLearning/AT01_MainMethodInJava.java
+++ src/test/java/JavaLearning/AT01_MainMethodInJava.java
@@ -7,5 +7,6 @@ package JavaLearning;
         //inside method we write code
         // this below code will print on console
         System.out.println("This is my First Java Class");
+            System.out.println("This is my First Java Class-3");
     }
 }
